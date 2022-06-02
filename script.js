57277bb59ef90f8123442bd59f3f82687be5f430
Ivan <ivan129522@gmail.com>
2022-06-02 21:22:34 +0300

    Add validation

--- script.js
+++ script.js
@@ -10,11 +10,26 @@ const personalMoviesDB = {
   private: false
 };
 
-for(let i = 0; i < 2; i++) {
+for (let i = 0; i < 2; i++) {
   const lastMovie = prompt('Один из последних просмотренных фильмов?', '');
   const lastMovieRate = prompt('На сколько оцените его?', '');
 
-  personalMoviesDB.movies[lastMovie] = lastMovieRate;
+  if (lastMovie != '' || lastMovieRate != '' ||
+    lastMovie != null || lastMovieRate != null ||
+    lastMovie.length < 50) {
+    personalMoviesDB.movies[lastMovie] = lastMovieRate;
+  } else {
+    i--;
+  }
+}
+
+if (personalMoviesDB.count < 10) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (personalMoviesDB.count >= 10 &&
+  personalMoviesDB.count < 30) {
+  alert('Вы классный зритель');
+} else {
+  alert('Вы киноман');
 }
 
 console.log(personalMoviesDB);
